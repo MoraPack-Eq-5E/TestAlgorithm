ae93396345fa23c0c170dcb97aa9c733a1a5c7ba
fapi19 <fabipra19@gmail.com>
2025-09-23 23:21:33 -0500

    testing

---
+++ src/main/java/com/grupo5e/morapack/algorithm/alns/Solucion.java
@@ -0,0 +1,17 @@
+package com.grupo5e.morapack.algorithm.alns;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.grupo5e.morapack.core.model.Aeropuerto;
+import com.grupo5e.morapack.core.model.Paquete;
+import com.grupo5e.morapack.core.model.Vuelo;
+
+public class Solucion {
+    private HashMap<Paquete, ArrayList<Vuelo>> solucion;
+    private ArrayList<HashMap<Paquete, ArrayList<Vuelo>>> espacioSoluciones;
+    private InputAeropuertos inputAeropuertos;
+    private ArrayList<Aeropuerto> aeropuertos;
+
+    public Solucion()''
+}
